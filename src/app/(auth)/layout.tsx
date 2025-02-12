bf88a3b8b61b8f509d2e47c04e9bf14802b26b2a
arcstep <43801@qq.com>
2025-02-12 21:02:39 +0800

    feat: next.js 的 layout 是一个嵌套树

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,16 +1,10 @@
-// src/app/layout.js
-'use client';
-
 import { JSX } from 'react';
 import '../../app/globals.css';
 
-export default function RootLayout({ children }: { children: JSX.Element }): JSX.Element {
+export default function AuthLayout({ children }: { children: JSX.Element }): JSX.Element {
     return (
-        <html lang="zh-CN">
-            <head />
-            <body>
-                <main>{children}</main>
-            </body>
-        </html>
+        <div className="min-h-screen bg-gray-100">
+            {children}
+        </div>
     );
 }
